0e9533beb7c46ea93bfcdedfccaed219fbb53778
wutchara02 <sstmuham@gmail.com>
2016-10-19 15:46:51 +0700

    show environment name

--- H_1StartProgect/config/application.rb
+++ H_1StartProgect/config/application.rb
@@ -11,5 +11,8 @@ module H1startprogect
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.time_zone = 'Central Time (US & Canada)'
+    config.active_record.schema_format = :ruby
+
   end
 end
